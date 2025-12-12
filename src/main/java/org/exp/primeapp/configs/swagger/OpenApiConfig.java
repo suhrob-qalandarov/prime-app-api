34dbba52807fd310ce347adb135fb2c3ca33e289
Suhrob <suhrob_kalandarov@outlook.com>
2025-12-12 19:03:57 +0500

    Make Swagger server URL configurable from application.properties
    - Add swagger.server.url property to application-prod.properties
    - Remove hardcoded localhost server URL from @OpenAPIDefinition
    - Add dynamic server URL configuration in OpenAPI bean
    - Default URL: https://api.gourmet.uz
    - Fixes CORS issue in Swagger UI by using correct server URL

--- src/main/java/org/exp/primeapp/configs/swagger/OpenApiConfig.java
+++ src/main/java/org/exp/primeapp/configs/swagger/OpenApiConfig.java
@@ -2,25 +2,32 @@ package org.exp.primeapp.configs.swagger;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @OpenAPIDefinition(
-        info = @Info(title = "PRIME77 e-commerce", version = "v2", description = "Online e-commerce webapp"),
-        servers = @Server(url = "http://localhost")
+        info = @Info(title = "PRIME77 e-commerce", version = "v2", description = "Online e-commerce webapp")
 )
 public class OpenApiConfig {
+
+    @Value("${swagger.server.url}")
+    private String serverUrl;
+
     @Bean
     public OpenAPI customOpenAPI() {
         final String SCHEME_NAME = "Authorization";
         return new OpenAPI()
+                .addServersItem(new Server()
+                        .url(serverUrl)
+                        .description("Production Server"))
                 .addSecurityItem(new SecurityRequirement().addList(SCHEME_NAME))
                 .components(new Components().addSecuritySchemes(SCHEME_NAME,
                         new SecurityScheme()
