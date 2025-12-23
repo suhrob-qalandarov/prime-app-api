5e18fef4df81fb3877189314ffd6db5fb25b08ee
Suhrob <suhrob_kalandarov@outlook.com>
2025-12-23 10:10:13 +0500

    Simplify CartItemReq by removing unnecessary JsonProperty annotations
    - Remove @JsonProperty annotations from CartItemReq record
    - Field names already match JSON keys, annotations are redundant
    - Cleaner and simpler code

--- src/main/java/org/exp/primeapp/models/dto/request/CartItemReq.java
+++ src/main/java/org/exp/primeapp/models/dto/request/CartItemReq.java
@@ -1,15 +1,8 @@
 package org.exp.primeapp.models.dto.request;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 public record CartItemReq(
-        @JsonProperty("productId")
         Long productId,
-        
-        @JsonProperty("productSize")
         String productSize,  // Size enum label yoki name (masalan "L" yoki "SIZE_41")
-        
-        @JsonProperty("productQuantity")
         Integer productQuantity
 ) {
 }
