835bd525eaa70b50578960138081f44cfaeec338
Suhrob <suhrob_kalandarov@outlook.com>
2026-01-03 10:59:04 +0500

    feat(user): update user service and repository logic

--- src/main/java/org/exp/primeapp/models/enums/AccountStatus.java
+++ src/main/java/org/exp/primeapp/models/enums/AccountStatus.java
@@ -1,4 +1,21 @@
 package org.exp.primeapp.models.enums;
 
-public enum UserStatus {
+import com.fasterxml.jackson.annotation.JsonValue;
+
+public enum AccountStatus {
+    ACTIVE("FAOL"),
+    INACTIVE("NOFAOL"),
+    DELETED("O'CHIRILGAN"),
+    BLOCKED("BLOKLANGAN");
+
+    private final String label;
+
+    AccountStatus(String label) {
+        this.label = label;
+    }
+
+    @JsonValue
+    public String getLabel() {
+        return label;
+    }
 }
