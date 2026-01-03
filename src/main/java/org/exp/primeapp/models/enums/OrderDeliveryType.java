4b343211e237acb77199e6bbe5a2c019954ed7d5
Suhrob <suhrob_kalandarov@outlook.com>
2026-01-03 21:39:30 +0500

    Fix: OrderDeliveryType deserialization issue and enum labels

--- src/main/java/org/exp/primeapp/models/enums/OrderDeliveryType.java
+++ src/main/java/org/exp/primeapp/models/enums/OrderDeliveryType.java
@@ -1,10 +1,11 @@
 package org.exp.primeapp.models.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
 
 public enum OrderDeliveryType {
-    VIA_TAXI("POCHTA"),
-    VIA_BTS("TAXI");
+    VIA_TAXI("TAXI"),
+    VIA_BTS("POCHTA");
 
     private final String label;
 
@@ -15,5 +16,19 @@ public enum OrderDeliveryType {
     @JsonValue
     public String getLabel() {
         return label;
+    }
+
+    @JsonCreator
+    public static OrderDeliveryType fromValue(String value) {
+        if (value == null) {
+            return null;
+        }
+        for (OrderDeliveryType type : values()) {
+            if (type.name().equalsIgnoreCase(value) || type.getLabel().equalsIgnoreCase(value)) {
+                return type;
+            }
+        }
+        throw new IllegalArgumentException(
+                "Unknown enum type " + value + ", Allowed values are " + java.util.Arrays.toString(values()));
     }
 }
